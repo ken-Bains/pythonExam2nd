b87bdae16751726934c04777a9e49d02da61ddab
Ken Bains <kennbains@gmail.com>
2017-01-05 21:25:26 -0800

    views

--- apps/mainProjectApp/views.py
+++ apps/mainProjectApp/views.py
@@ -30,25 +30,21 @@ def success(request):
 	if request.session.get('id'):
 		userWishlistIds =[]
 		userInfo = models.User.objects.filter(id = request.session['id'])
-
 		userWishlist = models.Wishlist.objects.all().filter(user__id = userInfo)
-
 		for ids in userWishlist:
 			userWishlistIds.append(ids.item.id)
-
 		items = models.Item.objects.all().exclude(id__in = userWishlistIds)
-
 		data = {"user":userInfo[0], "userWishlist": userWishlist, "items":items}
 		return render(request, "mainProjectApp/success.html", data)
 	return redirect('/')
 
 def add_item(request):
-	return render(request, 'mainProjectApp/add_item.html')
+	if request.session.get('id'):
+		return render(request, 'mainProjectApp/add_item.html')
+	return redirect('/')
 
 def add_item_db(request):
-
 	response = models.Item.objects.add_item_to_db(request.POST, request.session['id'])
-
 	if not response:
 		messages.success(request, "You have successfully added a item!")
 		return redirect('/success')
@@ -58,15 +54,16 @@ def add_item_db(request):
 		return redirect('/add_item')
 
 def add_to_wishlist(request, id):
-
 	item = models.Wishlist.objects.add_to_wishlist(id, request.session['id'])
 	return redirect('/success')
 
 def item_page(request, id):
-	users = models.Wishlist.objects.filter(item__id = id)
+	if request.session.get('id'):
+		users = models.Wishlist.objects.filter(item__id = id)
+		data = {"users":users, "item":users[0].item}
+		return render(request, 'mainProjectApp/item_page.html', data)
+	return redirect('/')
 
-	data = {"users":users, "item":users[0].item}
-	return render(request, 'mainProjectApp/item_page.html', data)
 
 def remove_wishlist(request, id):
 	models.Wishlist.objects.filter(item__id = id, user__id = request.session['id']).delete()
